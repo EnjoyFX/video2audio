5d2d58b2057f2b06b3a37c76f8e16914607e8ae2
AndyFX <vba2you@gmail.com>
2023-10-11 15:37:26 +0300

    initial commit

---
+++ video2audio.py
@@ -0,0 +1,61 @@
+import logging
+from tkinter import Tk
+from tkinter import filedialog as fd
+from moviepy import editor as me
+from plyer import notification as ntf
+import os
+import sys
+from logging import StreamHandler
+
+
+__version__ = '1.0.1'
+app_name = f'Py video->audio extractor (mp4->mp3) {__version__}'
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.DEBUG)
+handler = StreamHandler(stream=sys.stdout)
+logger.addHandler(handler)
+
+
+class Converter:
+    def __init__(self):
+        self.root = Tk()
+
+    def video_to_mp3(self, bitrate='320k'):
+        self.root.withdraw()
+
+        files = fd.askopenfilenames(filetypes=[('mp4 files', '*.mp4')])
+        if not files:
+            logger.info(f'{app_name}: No files selected')
+            sys.exit()
+
+        path = os.path.dirname(files[-1])
+        ok, bad = 0, 0
+        for file in files:
+            name = os.path.basename(file)
+            try:
+                video = me.VideoFileClip(file)
+            except Exception as e:
+                logger.warning(f'Issue with opening video "{name}": {e}')
+                sys.exit(1)
+
+            audio = video.audio
+            audioname = os.path.join(path, name.replace('.mp4', '.mp3'))
+
+            try:
+                audio.write_audiofile(audioname, bitrate=bitrate)
+                ok += 1
+            except Exception as e:
+                bad += 1
+                logger.warning(f'Issue with coonverting file "{name}": {e}')
+            finally:
+                video.close()
+
+        msg = f'Converted: {ok}, not converted: {bad}. ' \
+              f'Total files processed: {ok+bad}'
+        logger.info(msg)
+        ntf.notify(title=app_name, message=msg, timeout=10)
+
+
+if __name__ == "__main__":
+    c = Converter()
+    c.video_to_mp3()
